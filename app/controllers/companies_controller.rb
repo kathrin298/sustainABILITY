b8cce2aa82ec44fcc4a2313a1ac2716060628073
John Kealy <johnckealy@gmail.com>
2020-03-10 17:32:18 +0800

    basic pundit installed, not yet functioning

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,16 +3,19 @@ class CompaniesController < ApplicationController
 
   def index
     @companies = Company.all
+    @companies = policy_scope(Company).order(created_at: :desc)
   end
 
   def show; end
 
   def new
     @company = Company.new
+    authorize @company
   end
 
   def create
     @company = Company.new(company_params)
+    authorize @company
     @company.user = current_user
     if @company.save
       redirect_to company_path(@company)
