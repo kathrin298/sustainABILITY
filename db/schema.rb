8f709a3a525ca67a12848d78961a1b4662b8b064
John Kealy <johnckealy@gmail.com>
2020-03-12 11:14:32 +0800

    deleted socials partial

--- db/schema.rb
+++ db/schema.rb
@@ -185,17 +185,6 @@ ActiveRecord::Schema.define(version: 2020_03_11_104650) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "socials", force: :cascade do |t|
-    t.bigint "developer_id"
-    t.string "github"
-    t.string "linkedin"
-    t.string "twitter"
-    t.string "facebook"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["developer_id"], name: "index_socials_on_developer_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -231,5 +220,4 @@ ActiveRecord::Schema.define(version: 2020_03_11_104650) do
   add_foreign_key "messages", "users"
   add_foreign_key "questions", "jobs"
   add_foreign_key "reviews", "applications"
-  add_foreign_key "socials", "developers"
 end
