d2f1990f67a5e89e91dac512546a1e9849490164
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-03-16 11:40:36 +0800

    started implementing select2 form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,5 @@
 import "bootstrap";
 import "slick";
+import { initSelect2 } from '../plugins/init_select2';
+
+initSelect2();
---
+++ app/javascript/plugins/init_select2.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+import 'select2';
+
+const initSelect2 = () => {
+  $('.select2').select2();
+
+export { initSelect2 };
