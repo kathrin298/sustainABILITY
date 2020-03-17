e13b4c1d33a12ce6bf910918b090caa2fbe24a5a
John Kealy <johnckealy@gmail.com>
2020-03-17 14:13:10 +0800

    completed filtering for jobs with accepted applications and developers unavailable

---
+++ app/javascript/plugins/developer_filter.js
@@ -0,0 +1,23 @@
+const hideDevs = () => {
+  let unavailable = document.querySelectorAll('[data-hireable="Not available"]');
+  unavailable.forEach(dev => {
+    dev.classList.toggle('d-none');
+  });
+  let numberOfResults = document.querySelectorAll(".serp-card-holder").length;
+  let numberOfHiddens = document.querySelectorAll(".d-none").length;
+
+  document.getElementById("serp_headline").innerText = `Showing ${numberOfResults - numberOfHiddens} developers`
+};
+
+
+const initDevFilter = () => {
+  let checkbox = document.getElementById("serpcheckbox");
+  checkbox.checked = true;
+  hideDevs();
+
+  checkbox.addEventListener('change', function() {
+      hideDevs();
+  });
+}
+
+export { initDevFilter, hideDevs };
